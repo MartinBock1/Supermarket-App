b8a9e20841173ba11506fd25d927719cd637106b
Martin Bock <martinbock@arcor.de>
2025-05-15 08:47:49 +0200

    "serializer.py created"

---
+++ supermarket_app/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class MarketSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    location = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
